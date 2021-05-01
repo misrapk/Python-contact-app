f4911730311652260922e04461c486952deec2bb
pkmisra <pkmisra1999@gmail.com>
2021-05-02 00:46:36 +0530

    final Code Added

--- contacts.py
+++ contacts.py
@@ -19,6 +19,7 @@ class Contacts:
         self.createTreeView()
         self.createScrollBar()
         self.createBottomButton()  
+        self.viewRecords()
      
      #function that interacts with database   
     def executeDbQuery(self, query, parameters=()):
@@ -52,7 +53,7 @@ class Contacts:
         
         self.numfield = Entry(labelframe)
         self.numfield.grid(row=3, column=2, sticky=W, padx=5, pady=2)
-        Button(labelframe, text='Add Contact', command=2,bg = "blue", fg = 'white').grid(row=4, column =2, sticky=E, pady=2, padx=15)
+        Button(labelframe, text='Add Contact', command= self.onAddContactClicked,bg = "blue", fg = 'white').grid(row=4, column =2, sticky=E, pady=2, padx=15)
         
         
         
@@ -74,20 +75,117 @@ class Contacts:
         
         
     def createBottomButton(self):
-        Button(text='Delete Selected', command="", bg="red", fg="white").grid(row=8, column=0, sticky=W, pady=10, padx=20)
-        Button(text='Modify Selected', command="", bg="purple", fg="white").grid(row=8, column=2, sticky=W)
+        Button(text='Delete Selected', command=self.onDeleteButtonClicked, bg="red", fg="white").grid(row=8, column=0, sticky=W, pady=10, padx=20)
+        Button(text='Modify Selected', command=self.onModifySelected, bg="purple", fg="white").grid(row=8, column=2, sticky=W)
+    
+    def onAddContactClicked(self):
+        self.addnewContact()
         
+    def onDeleteButtonClicked(self):
+        self.message['text'] = ''
+        try:
+            self.tree.item(self.tree.selection())['values'][0]
+        except IndexError as e:
+            self.message['text'] = "Please select atleast one contact to delete!"
+            return 
+        self.deleteRecords()
+        
+    def onModifySelected(self):
+        self.message['text'] = ''
+        try:
+            self.tree.item(self.tree.selection())['values'][0]
+        except IndexError as e:
+            self.message['text'] = "Nothing is selected to Modify"
+            return 
+        self.openModifyWindow()
+      
     def addnewContact(self):
         if self.newContactsValidated():
-            query = "INSERT INTO contacts"            
+            query = 'INSERT INTO contactList VALUES(NULL,?,?,?)'        
+            parameters = (self.namefield.get(), self.emailfield.get(), self.numfield.get())
+            self.executeDbQuery(query, parameters)
             
-    def newContactsValidate(self):
-        return len(self.namefield.get()) !=0 and len(self.emailfield.get()) !=0 and len(self.numfield.get()) !=-0
+            #message after a contact is added
+            self.message['text'] = "New Contact {} added".format(self.namefield.get())
+            
+            #checks that record is cleared before entring or not
+            self.emailfield.delete(0, END) 
+            self.namefield.delete(0, END)
+            self.numfield.delete(0, END)
+            self.viewRecords()
+            
+        else:
+            self.message['text'] = "Fields can't be blank!"
+            self.viewRecords()   #fuction that fetch record from db and display
+            
+            
+    #this function checks if the input is enetered or not
+    def newContactsValidated(self):
+        return len(self.namefield.get()) !=0 and len(self.emailfield.get()) !=0 and len(self.numfield.get()) != 0
+    
+    
+    #function to fetch the data from db and display
+    def viewRecords(self):
+        items = self.tree.get_children()
+        for item in items:
+            self.tree.delete(item)
+            
+        query = 'SELECT * FROM contactList ORDER BY name desc'
+        contactEntries = self.executeDbQuery(query)
+        for row in contactEntries:
+            #row 1 is name 2 is email and 3 is number
+            self.tree.insert('', 0, text=row[1], values=(row[2], row[3]))
+        
+    #TODO: function to delete Records
+    def deleteRecords(self):
+        self.message['text'] = ''
+        name = self.tree.item(self.tree.selection())['text']
+        query = 'DELETE FROM contactList WHERE name = ?'
+        self.executeDbQuery(query, (name,))
+        self.message['text'] = 'Contacts for {} Succesfully Deleted!'.format(name)
+        self.viewRecords()
+        
+    #TODO: function to modify the selected record
+    #to open the window
+    def openModifyWindow(self):
+        name = self.tree.item(self.tree.selection())['text']
+        oldNumber = self.tree.item(self.tree.selection())['values'][1]
+        self.transient = Toplevel()
+        self.transient.title("Update Contact")
+        Label(self.transient, text = 'Name: ').grid(row=0, column=1)
+        Entry(self.transient, textvariable=StringVar(self.transient, value = name), state = 'readonly').grid(row=0, column=2)
+        Label(self.transient, text = 'Old Contact number: ').grid(row=1, column=1)
+        Entry(self.transient, textvariable=StringVar(self.transient, value = oldNumber), state = 'readonly').grid(row=1, column=2)
+        
+        Label(self.transient, text = 'New phone Number:  ').grid(row=2, column=1)
+        newPhoneEntryWidget =  Entry(self.transient)
+        newPhoneEntryWidget.grid(row =2, column=2)
+        
+        # Button(labelframe, text='Add Contact', command= self.onAddContactClicked,bg = "blue", fg = 'white').grid(row=4, column =2, sticky=E, pady=2, padx=15)
+        
+        Button(self.transient, text = 'Update Contact', command=lambda: self.updateContact(
+            newPhoneEntryWidget.get(), oldNumber, name
+        ), bg = "blue",fg="white" ).grid(row=3, column=2, sticky=E)
+        
+        self.transient.mainloop()
+        
+    #function to update contact (query)
+    def updateContact(self, newPhone, oldPhone, name):
+        query = 'UPDATE contactList SET number = ? WHERE number =? AND name = ?'
+        parameters = (newPhone, oldPhone, name)
+        self.executeDbQuery(query, parameters)
+        self.transient.destroy()
+        self.message['text'] = 'Phone Number of {} is modified Successfull!'.format(name)
+        self.viewRecords() 
+        
+        
     
         
 if __name__ == '__main__':
     root=Tk()
     root.title('Contact List')
+    root.geometry("650x450")
+    root.resizable(width=False, height=False)
     application = Contacts(root)
     root.mainloop()
     
